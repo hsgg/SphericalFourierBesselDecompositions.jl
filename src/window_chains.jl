9c492aaca52263aa1bb319587f00505571fc4e79
Henry Gebhardt <henry.s.gebhardt@jpl.nasa.gov>
2020-12-09 02:23:03 -0800

    window_chains: k=1 tests pass

--- src/window_chains.jl
+++ src/window_chains.jl
@@ -11,38 +11,50 @@ using ..HealPy
 using ..NDIterators
 using ..SeparableArrays
 using ..WignerChains
+using ..Modes
 
 
-function I_nlnl2ll(I_LM_ln_ln, ll, nn)
-    I_LM_l_l = fill(im*NaN, size(I_LM_ln_ln,1), length(ll), length(ll))
-    for j=1:length(ll), i=1:length(ll)
-        I_LM_l_l[:,i,j] .= I_LM_ln_ln[:,i,nn[i],j,nn[j]]
-    end
-    return I_LM_l_l
+# This struct encodes how to access I_LM_lᵢnᵢ_lⱼnⱼ such that we can access it as I[LM,i,j].
+struct NeqLView{T}
+    I_LM_ln_ln::Array{T,5}
+    ell::Array{Int,1}
+    n1::Array{Int,1}
+    n2::Array{Int,1}
 end
+NeqLView(I, ell, n1, n2) = NeqLView(I, convert(Array{Int}, ell), convert(Array{Int}, n1), convert(Array{Int}, n2))
 
-function window_chain(k, win, wmodes, cmodes)
-    I_LM_ln_ln = calc_I_LM_nl_nl(win, wmodes, cmodes)
-    lmax = cmodes.amodes.lmax
-    nmax = cmodes.amodes.nmax
-    Wk_ll_nn = fill(im*NaN, fill(lmax+1,k)..., fill(nmax,k)...)
-    ll = NDIterator(0, lmax; N=k)
-    while advance(ll)
-        nmax_l = @. cmodes.nmax_l[ll+1]
-        nn = NDIterator(1, nmax_l)
-        while nn
-            Wk_ll_nn[ll..., nn...] = window_chain(ll, nn, I_LM_ln_ln)
+Base.getindex(v::NeqLView, LM::Int, i::Int, j::Int) = v.I_LM_ln_ln[LM, v.ell[i]+1, v.n1[i], v.ell[j]+1, v.n2[j]]
+
+
+
+function window_chain(k, win, wmodes::ConfigurationSpaceModes, cmodes::ClnnModes)
+    amodes = cmodes.amodes
+    I_LM_ln_ln, LMcache = calc_I_LM_nl_nl(win, wmodes, amodes)
+    lmax = amodes.lmax
+    nmax = amodes.nmax
+    lnnsize = getlnnsize(cmodes)
+    Wk_lnni = fill(NaN, fill(lnnsize,k)...)
+    lnn = NDIterator(1, lnnsize; N=k)
+    while advance(lnn)
+        ll = Array{Int}(undef, k)
+        nn1 = Array{Int}(undef, k)
+        nn2 = Array{Int}(undef, k)
+        for i=1:k
+            l, n1, n2 = getlnn(cmodes, lnn[i])
+            ll[i] = l
+            nn1[i] = n1
+            nn2[i] = n2
         end
+        Wk_lnni[lnn...] = window_chain(ll, nn1, nn2, I_LM_ln_ln, LMcache)
     end
-    return Wk_ll_nn
+    return Wk_lnni
 end
-function window_chain(ell, nn, I_LM_ln_ln)
-    I_LM_l_l = I_nlnl2ll(I_LM_ln_ln, ell, nn)
-    return window_chain(ell, I_LM_l_l)
+function window_chain(ell, n1, n2, I_LM_ln_ln, LMcache)
+    I_LM_l_l = NeqLView(I_LM_ln_ln, ell, n2, n1)
+    return window_chain(ell, I_LM_l_l, LMcache)
 end
-function window_chain(ell, I_LM_l_l)
-    T = Complex{Float64}
-    #@assert length(ell) == length(nn)
+function window_chain(ell, I_LM_l_l, LMcache)
+    T = Float64
     k = length(ell)
 
     # only iterate over triangles
@@ -54,28 +66,39 @@ function window_chain(ell, I_LM_l_l)
         Lmin[i] = abs(ell[i] - ell[i-1])
         Lmax[i] = ell[i] + ell[i-1]
     end
-    @show Lmin Lmax
+    #@show Lmin Lmax
 
     wk = T(0)
     L = NDIterator(Lmin, Lmax)
     while advance(L)
-        twoLplus1_w3j000 = (2*L[1] + 1) * wigner3j(T, ell[end], ell[1], L[1], 0, 0, 0)
+        twoLplus1_w3j000 = √(2*L[1] + 1) * wigner3j(T, ell[end], ell[1], L[1], 0, 0, 0)
         for i=2:k
-            twoLplus1_w3j000 *= (2*L[i] + 1) * wigner3j(T, ell[i-1], ell[i], L[i], 0, 0, 0)
+            twoLplus1_w3j000 *= √(2*L[i] + 1) * wigner3j(T, ell[i-1], ell[i], L[i], 0, 0, 0)
         end
 
         # sum over M[i]
         M = NDIterator(-L, L)
         while advance(M)
             w3jk = wigner3j_chain(ell, L, M)
+	    #@show L,M,w3jk
             (w3jk == 0) && continue
 
-            Iprod = get_I(I_LM_l_l, L[1], M[1], ell[end], ell[1])
+            LM = LMcache[L[1]+1][abs(M[1])+1]
+            I = I_LM_l_l[LM, k, 1]
+            (M[1] < 0) && (I = conj(I))
+            #@show L[1],M[1],I
+            Iprod = I
             for i=2:k
-                Iprod *= get_I(I_LM_l_l, L[i], M[i], ell[i-1], ell[i])
+                LM = LMcache[L[i]+1][abs(M[i])+1]
+                I = I_LM_l_l[LM, i-1, i]
+                (M[i] < 0) && (I = conj(I))
+                Iprod *= I
+                #@show L[i],M[i],I
             end
 
-            wk += twoLplus1_w3j000 * Iprod * w3jk
+            dwk = twoLplus1_w3j000 * real(Iprod) * w3jk  # imag must sum out
+            wk += dwk
+	    #@show L,M,dwk,Iprod
         end
     end
     wk *= (4*π)^(-k/2) * prod(@. 2 * ell + 1)
@@ -88,7 +111,7 @@ end
 function calc_Wr_lm(win, LMAX, Wnside)
     nr = size(win,1)
     Wr_lm = fill(NaN*im, nr, getlmsize(LMAX))
-    @time for i=1:nr
+    for i=1:nr
         W = hp.ud_grade(win[i,:], Wnside)
         Wr_lm[i,:] .= hp.map2alm(W, lmax=LMAX)
     end
@@ -110,7 +133,7 @@ function calc_radial_mixing(lmax, nmax_l, gnlr, phi, r, Δr)
         for l=0:lmax, n=1:nmax_l[l+1]
             !isnan(gnlgNLϕ[n,l+1,N,L+1]) && continue
             # sanity check
-            check_radial_integral_convergence(n, l, N, L, r, Δr, cmodes)
+            check_radial_integral_convergence(n, l, N, L, r, Δr, amodes)
             @. ggϕint = r^2 * gnlr[:,n,l+1] * gnlr[:,N,L+1] * phi
             gg = Δr * sum(ggϕint)
             gnlgNLϕ[n,l+1,N,L+1] = gg
@@ -121,44 +144,48 @@ function calc_radial_mixing(lmax, nmax_l, gnlr, phi, r, Δr)
 end
 
 
-function check_radial_integral_convergence(n1, l1, n2, l2, r, Δr, cmodes)
+function check_radial_integral_convergence(n1, l1, n2, l2, r, Δr, amodes)
     Nsamp = 8 * (n1 + n2) + l1 + l2
     if Nsamp > length(r)
-        @error "Radial integral may not converge" Nsamp length(r) n1 l1 n2 l2 cmodes.amodes.knl[n,l+1] cmodes.amodes.knl[N,L+1] cmodes.amodes.rmin cmodes.amodes.rmax extrema(r) Δr
+        @error "Radial integral may not converge" Nsamp length(r) n1 l1 n2 l2 amodes.knl[n,l+1] amodes.knl[N,L+1] amodes.rmin amodes.rmax extrema(r) Δr
         throw(ErrorException("Nsamp > nr"))
     end
 end
 
 
-function calc_I_LM_nl_nl(win, wmodes, cmodes)
-    #cmodes = bcmodes.cmodes
-    amodes = cmodes.amodes
+function calc_I_LM_nl_nl(win, wmodes, amodes)
+    lmax = amodes.lmax
+    nmax = amodes.nmax
+    nmax_l = amodes.nmax_l
 
     # Wr_lm
     LMAX = 2 * amodes.lmax
-    Wr_lm = calc_Wr_lm(win, LMAX, Wnside)
+    Wr_lm = calc_Wr_lm(win, LMAX, wmodes.nside)
     LMcache = [hp.Alm.getidx.(LMAX, L, 0:L) .+ 1 for L=0:LMAX]
+    lmsize = hp.Alm.getsize(LMAX)
+    #@show LMcache
+    #@show typeof(LMcache)
 
     # gnlr
     r, Δr = window_r(wmodes)
     gnl = amodes.basisfunctions
     gnlr = fill(NaN, length(r), size(gnl.knl)...)
-    @time for l=0:amodes.lmax, n=1:amodes.nmax_l[l+1]
+    for l=0:amodes.lmax, n=1:amodes.nmax_l[l+1]
         @. gnlr[:,n,l+1] = gnl(n,l,r)
     end
 
     # I_LM_ln_ln
-    I_LM_ln_ln = fill(im*NaN, length(LMcache), lmax+1, nmax, lmax+1, nmax)
-    for l2=0:lmax, n2=1:nmax_l[l2], l1=l2:lmax, n1=n2:nmax_l[l1], iLM=1:length(LMcache)
-        LM = LMcache[iLM]
-        !isnan(I_LM_ln_ln[LM,l1+1,n1,l2+1,n2]) && continue
-        check_radial_integral_convergence(n1, l1, n2, l2, r, Δr, cmodes)
-        I = Δr * sum(@. r^2 * gnlr[:,n1,l1+1] * gnlr[:,n2,l2+1] * Wr_lm[:,LM])
-        I_LM_ln_ln[LM,l1+1,n1,l2+1,n2] = I
-        I_LM_ln_ln[LM,l2+1,n2,l1+1,n1] = I
+    I_LM_ln_ln = fill(im*NaN, lmsize, lmax+1, nmax, lmax+1, nmax)
+    for l2=0:lmax, n2=1:nmax_l[l2+1], l1=0:lmax, n1=1:nmax_l[l1+1], iLM=1:lmsize
+        #@show l2, n2, l1, n1, iLM, lmsize
+        !isnan(I_LM_ln_ln[iLM,l1+1,n1,l2+1,n2]) && continue
+        check_radial_integral_convergence(n1, l1, n2, l2, r, Δr, amodes)
+        I = Δr * sum(@. r^2 * gnlr[:,n1,l1+1] * gnlr[:,n2,l2+1] * Wr_lm[:,iLM])
+        I_LM_ln_ln[iLM,l1+1,n1,l2+1,n2] = I
+        I_LM_ln_ln[iLM,l2+1,n2,l1+1,n1] = I
     end
 
-    return I_LM_ln_ln
+    return I_LM_ln_ln, LMcache
 end
 
 
--- test/test_window_chains.jl
+++ test/test_window_chains.jl
@@ -1,10 +1,139 @@
 #!/usr/bin/env julia
 
 
+using SphericalFourierBesselDecompositions
+SFB = SphericalFourierBesselDecompositions
+
+using Test
+
+
 @testset "Window Chains" begin
-    ell = [1, 1, 1, 1]
-    I_LM_nl1_n12 = []
-    SFB.window_chains.window_chain(ell, I_LM_nl1_n12)
+    false && @testset "k = 1" begin
+        k = 1
+        rmin = 500.0
+        rmax = 1000.0
+        amodes = SFB.AnlmModes(3, 5, rmin, rmax)
+        cmodes = SFB.ClnnModes(amodes, Δnmax=1)
+        wmodes = SFB.ConfigurationSpaceModes(rmin, rmax, 1000, amodes.nside)
+        win = SFB.make_window(wmodes, :radial, :ang_quarter)
+
+        # single modes
+        I_LM_ln_ln, LMcache = SFB.window_chains.calc_I_LM_nl_nl(win, wmodes, amodes)
+        ell = [1]
+        n1 = [1]
+        n2 = [1]
+        I_LM_l_l = SFB.window_chains.NeqLView(I_LM_ln_ln, ell, n1, n2)
+        wk = SFB.window_chain(ell, I_LM_l_l, LMcache)
+        @show wk
+        @test wk ≈ 0.08538664002556066 * (2*ell[1]+1) rtol=1e-6
+
+
+        # all modes
+        wk_lnni = SFB.window_chain(k, win, wmodes, cmodes)
+        @show wk_lnni
+        @test all(isreal.(wk_lnni))
+        wk_lnni = real.(wk_lnni)
+
+        wlnn = SFB.win_lnn(win, wmodes, cmodes)
+        @show wlnn
+        @show size(wk_lnni) size(wlnn)
+        for i=1:SFB.getlnnsize(cmodes)
+            l, n1, n2 = SFB.getlnn(cmodes, i)
+            wk = wk_lnni[i] / (2*l+1)
+            @show l,n1,n2,wk_lnni[i],wk,wlnn[i]
+            @test wk ≈ wlnn[i] rtol=1e-6
+        end
+    end
+
+
+    @testset "k = 2" begin
+        k = 2
+        rmin = 500.0
+        rmax = 1000.0
+        amodes = SFB.AnlmModes(2, 2, rmin, rmax)
+        cmodes = SFB.ClnnModes(amodes, Δnmax=1)
+        wmodes = SFB.ConfigurationSpaceModes(rmin, rmax, 1000, amodes.nside)
+        win = SFB.make_window(wmodes, :radial, :ang_quarter)
+
+        # comparison
+        @time M = SFB.power_win_mix(win, wmodes, cmodes)
+        @time M = SFB.power_win_mix(win, wmodes, cmodes)
+
+        # single mode
+        I_LM_ln_ln, LMcache = SFB.window_chains.calc_I_LM_nl_nl(win, wmodes, amodes)
+        ell = [1, 1]
+        n  = [1, 1]
+        n′ = [1, 1]
+        I_LM_l_l = SFB.window_chains.NeqLView(I_LM_ln_ln, ell, n, n′)
+        @show ell,n,n′
+        wk = SFB.window_chain(ell, I_LM_l_l, LMcache)
+        lnnA = SFB.getidx(cmodes,ell[2],n′[2],n[2])
+        lnnB = SFB.getidx(cmodes,ell[1],n[1],n′[1])
+        Mab = M[lnnA, lnnB]
+        @show wk Mab*(2*ell[2]+1)
+        @test wk ≈ Mab * (2*ell[2]+1)
+
+        # another mode
+        I_LM_ln_ln, LMcache = SFB.window_chains.calc_I_LM_nl_nl(win, wmodes, amodes)
+        ell = [2, 0]
+        n  = [1, 1]
+        n′ = [2, 1]
+        I_LM_l_l = SFB.window_chains.NeqLView(I_LM_ln_ln, ell, n, n′)
+        @show ell,n,n′
+        wk = SFB.window_chain(ell, I_LM_l_l, LMcache)
+        lnnA = SFB.getidx(cmodes,ell[2],n′[2],n[2])
+        lnnB = SFB.getidx(cmodes,ell[1],n[1],n′[1])
+        Mab = M[lnnA, lnnB]
+        @show wk Mab*(2*ell[2]+1)
+        @test wk ≈ Mab * (2*ell[2]+1)
+
+        # yet another mode
+        I_LM_ln_ln, LMcache = SFB.window_chains.calc_I_LM_nl_nl(win, wmodes, amodes)
+        ell = [0, 0]
+        n  = [1, 1]
+        n′ = [1, 2]
+        I_LM_l_l = SFB.window_chains.NeqLView(I_LM_ln_ln, ell, n, n′)
+        @show ell,n,n′
+        wk = SFB.window_chain(ell, I_LM_l_l, LMcache)
+        lnnA = SFB.getidx(cmodes,ell[2],n′[2],n[2])
+        lnnB = SFB.getidx(cmodes,ell[1],n[1],n′[1])
+        Mab = M[lnnA, lnnB]
+        @show wk Mab*(2*ell[2]+1)
+        @test wk ≈ Mab * (2*ell[2]+1)
+
+        # guess what? another mode
+        I_LM_ln_ln, LMcache = SFB.window_chains.calc_I_LM_nl_nl(win, wmodes, amodes)
+        ell = [0, 0]
+        n  = [1, 1]
+        n′ = [2, 2]
+        I_LM_l_l = SFB.window_chains.NeqLView(I_LM_ln_ln, ell, n, n′)
+        @show ell,n,n′
+        wk = SFB.window_chain(ell, I_LM_l_l, LMcache)
+        lnnA = SFB.getidx(cmodes,ell[2],n′[2],n[2])
+        lnnB = SFB.getidx(cmodes,ell[1],n[1],n′[1])
+        Mab = M[lnnA, lnnB]
+        @show wk Mab*(2*ell[2]+1)
+        @show wk/(Mab*(2*ell[2]+1))
+        @test wk ≈ Mab * (2*ell[2]+1)
+
+
+        ## all modes
+        #@time wk_lnni = SFB.window_chain(k, win, wmodes, cmodes)
+        #@time wk_lnni = SFB.window_chain(k, win, wmodes, cmodes)
+        #@test all(isreal.(wk_lnni))
+        #wk_lnni = real.(wk_lnni)
+
+        #@show size(wk_lnni) size(M)
+        #@test size(wk_lnni) == size(M)
+        #for i2=1:SFB.getlnnsize(cmodes), i1=1:SFB.getlnnsize(cmodes)
+        #    l1, n1, n1′ = SFB.getlnn(cmodes, i1)
+        #    l2, n2, n2′ = SFB.getlnn(cmodes, i2)
+        #    i2′ = SFB.getidx(cmodes, l2, n2′, n2)
+        #    wk = wk_lnni[i1,i2′] / (2*l2+1)
+        #    @show l1,n1,n1′,l2,n2,n2′,wk,M[i2,i1]
+        #    @test wk ≈ M[i2,i1]
+        #end
+    end
 end
 
 
