61743aeef5a45493f9768f4728e22c05312d4d30
Henry Gebhardt <gebhardt@caltech.edu>
2023-01-31 22:54:53 -0800

    test/calc_cmix.jl: things change quickly

--- test/calc_cmix.jl
+++ test/calc_cmix.jl
@@ -23,31 +23,10 @@ using Profile, FlameGraphs, ProfileView
     println("Make a window...")
     win1 = @time SFB.make_window(wmodes, :radial, :ang_quarter)
 
-
-    cmix = SFB.power_win_mix(win1, win1, wmodes, cmodes)
-
-
-    println("Calculate r √Δr gnl...")
-    r, Δr = SFB.window_r(wmodes)
-    rsdrgnlr = @time r .* .√Δr .* SFB.Windows.precompute_gnlr(amodes, wmodes)
-
-    println("Calculate W_lm(r)...")
-    LMAX = 2 * amodes.lmax
-    W1r_lm, L1M1cache = SFB.Windows.optimize_Wr_lm_layout(SFB.Windows.calc_Wr_lm(win1, LMAX, amodes.nside), LMAX)
-
-    println("calc_cmixii_old...")
     lnnsize = SFB.getlnnsize(cmodes)
-    @show lnnsize
-    i = lnnsize
-    i′ = lnnsize
-    gg1 = Array{Float64,1}(undef, nr)
-    gg2 = Array{Float64,1}(undef, nr)
-    @time SFB.Windows.calc_cmixii_old(i, i′, cmodes, rsdrgnlr, W1r_lm, W1r_lm, L1M1cache, false, false, gg1, gg2)
-    @time SFB.Windows.calc_cmixii_old(i, i′, cmodes, rsdrgnlr, W1r_lm, W1r_lm, L1M1cache, false, false, gg1, gg2)
-    @btime SFB.Windows.calc_cmixii_old($i, $i′, $cmodes, $rsdrgnlr, $W1r_lm, $W1r_lm, $L1M1cache, false, false, $gg1, $gg2)
 
-    #println("calc_cmix()...")
-    SFB.Windows.calc_cmix(cmodes, rsdrgnlr, W1r_lm, W1r_lm, L1M1cache, false, false)
-    @profview SFB.Windows.calc_cmix(cmodes, rsdrgnlr, W1r_lm, W1r_lm, L1M1cache, false, false)
+    cmix = SFB.power_win_mix(win1, win1, wmodes, cmodes, lnn_min=lnnsize-1)
+
+    @profview SFB.power_win_mix(win1, win1, wmodes, cmodes, lnn_min=lnnsize-10)
 
 #end
